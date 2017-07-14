2eacabf651e3d1d9e5fc40327d7a21c55ce81bed
sudhirnikhade <sudhirnikhade@gmail.com>
2017-07-14 10:26:30 +0530

    1st module coding is completed and ready to release.

--- hello.c
+++ hello.c
@@ -2,4 +2,4 @@ This is the inital commit and first line initiated by master.
 
 the second release is started to write now by team 1. This team is writing 2nd module.
 fixing bugs in release 1 and found ok.
-
+2nd release module ready.
